8a5c50b51e52a31fe63b06b894055a64243b3834
aikido <ladunnikita02@gmail.com>
2026-08-28 20:24:36 +0300

    Added JWTServiceTest

---
+++ src/test/java/com/akido/orderservice/services/OrderServiceTest.java
@@ -0,0 +1,28 @@
+package com.akido.orderservice.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+    @Test
+    void getAllOrders() {
+    }
+
+    @Test
+    void getUserOrders() {
+    }
+
+    @Test
+    void createOrder() {
+    }
+
+    @Test
+    void updateOrderStatus() {
+    }
+
+    @Test
+    void deleteOrder() {
+    }
+}
